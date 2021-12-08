d34b49a8bf91a3ad6f7e4a1f535ac3417279a4bd
GustavoNeiraGonzalez <gustavoeduardoneiragonzalez@gmail.com>
2021-12-08 12:25:13 -0300

    agregado settings un middleware en app.js pequeño avanze configuracion mysql

--- src/app.js
+++ src/app.js
@@ -1,6 +1,23 @@
 const express = require('express');
-const app = express();
+const path = require('path');
+const morgan = require('morgan');
+const mysql = require('mysql');
 
-app.listen(3000, () =>{
+
+const app = express();
+//settings
+app.set('port', process.env.PORT || 3000) //significa que revise el puerto
+//en el sistema operativo y si no existe, entonces que cree el puerto 3000
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname,'views'))
+//settings
+
+//middlewares (ejecutar funciones primero)
+app.use(morgan('dev'));
+
+
+//middlewares
+
+app.listen(app.get('port'), () =>{
     console.log('Server on port 3000');
 })
